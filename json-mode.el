483bacfca90cf7d3bf380056e7dc7209698ea37c
Yasuyuki Oka <yasuyk@gmail.com>
2013-10-16 07:47:08 +0900

    Fix library header according to the GNU Emacs Manual
    - Add Commentary section
    - Add Code section
    - Add Copyright section
    - Add License
    
    See URL http://www.gnu.org/software/emacs/manual/html_node/elisp/Simple-Packages.html

--- json-mode.el
+++ json-mode.el
@@ -1,10 +1,29 @@
 ;;; json-mode.el --- Major mode for editing JSON files
-;;; Author: Josh Johnston
-;;; URL: https://github.com/joshwnj/json-mode
-;;; Version: 1.1.0
 
-;;;;
+;; Copyright (C) 2011-2013 Josh Johnston
+
+;; Author: Josh Johnston
+;; URL: https://github.com/joshwnj/json-mode
+;; Version: 1.1.0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
 ;; extend the builtin js-mode's syntax highlighting
+
+;;; Code:
 
 (defvar json-mode-hook nil)
 
